12d37e7632ac8525b7159a1658ba6aaed583e27b
victor <82973437+chipingo42@users.noreply.github.com>
2023-04-28 00:34:38 +0100

    change add

--- app.js
+++ app.js
@@ -44,6 +44,4 @@ app.use((err, req, res, next) => {
   })
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port 5556`);
-})
+app.listen(PORT, () =>  console.log(`Server running on port 5556`))
